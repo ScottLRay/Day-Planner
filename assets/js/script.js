fa57bcdc57896851056c5cf87c638e03bd42dce3
unname666 <78314985+unname666@users.noreply.github.com>
2021-03-08 08:30:06 -0700

    working on readme

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,73 +1,60 @@
+$(document).ready(function () {
+  // using a tool called moment to get the current date
+  const today = moment().format("MMM Do, YYYY");
+  $("#currentDay").text(today);
+  // using moment to get the current hour
+  let currentHour = +moment().format("H");
 
-$(document).ready(function(){
+  //linking to the html side
+  const $timeSlots = $("#time-slot");
 
-
-
-// using a tool called moment to get the current date
-const today = moment().format("MMM Do, YYYY");
-$("#currentDay").text(today);
-// using moment to get the current hour
-let currentHour = +moment().format("H");
-
-console.log(currentHour)
-
-const $timeSlots = $("#time-slot");
-
-const arrHour = [9,10,11,12,1,2,3,4,5]
-// debugger
-
-for (let i = 5; i < 17; i++) {
-  $timeSlots.append(buildTimeSlot(i));
-//   formatHour.currentHour.append()
-}
-
-// function formatHour(){
-//     for (let i = 0; i < arrHour.length; i++) {
-//         // const thisHour = arrHour[i]
-//          $(".hour").append(arrHour[i])
-    
-//         return   
-//     }
-    
-// }
-// formatHour()
-
-
-function buildTimeSlot(hour) {
   
-    var background;
-  if (currentHour === hour) {
-    background = ".present";
-  } else if (currentHour > hour) {
-    background = ".past";
-  } else {
-    background = ".future";
+  // a for loop, looping over numbers from 5 to 17
+  for (let i = 5; i <= 17; i++) {
+    $timeSlots.append(buildTimeSlot(i));
   }
 
-  var $timeSlots = $("<div>")
-    .addClass("row time-block")
-    .attr("id", `hour-${hour}`);
-  var $hour = $("<div>").addClass("col-md-1 hour").text(currentHour)
-  var $textArea = $("<textarea>").addClass(
-    "col-md-10 description" + background
-  );
-  var $button = $("<button>")
-    .addClass("btn saveBtn col-md-1")
-    .append($("<i>").addClass("fas fa-save"));
-   return $timeSlots.append($hour, $textArea, $button);
-}
+  // function of building the html in js
+  function buildTimeSlot(hour) {
+    // if statements showing past, present and future in differnet colors
+    var background = "background-color";
+    if (currentHour === hour) {
+      background = "present";
+    } else if (currentHour > hour) {
+      background = "past";
+    } else {
+      background = "future";
+    }
+    // actual js code building html
+    var $timeSlots = $("<div>")
+      .addClass("row time-block")
+      .attr("id", `hour-${hour}`);
+    var $hour = $("<div>").addClass("col-md-1 hour").text(hour);
+    var $textArea = $("<textarea>").addClass(
+      "col-md-10 description " + background
+    );
+    var $button = $("<button>")
+      .addClass("btn saveBtn col-md-1")
+      //fas and fas-save is the save btn icon
+      .append($("<i>").addClass("fas fa-save"));
+    //appending the code to the page
+    return $timeSlots.append($hour, $textArea, $button);
+    
+  }
 
-// $(".fa-save").on("click", function(e){
-//     e.preventDefault();
-//     localStorage.setItem("description", JSON.stringify)
-// })
-})
+   var description = document.querySelectorAll(".description")
+  // save the endusers entrys
+  $(".saveBtn").on("click", function (e) {
+    e.preventDefault();
+    localStorage.setItem("description", JSON.stringify(description));
+    
+  });
+  console.log(localStorage);
 
 
+// localStorage.getItem("description", JSON.parse(val))
 
 
-
-
-
+});
 
 
